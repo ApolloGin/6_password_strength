863648e7b9e77db33b090b2f38119fdcfa80f9f6
Toxagin <toxagin@gmail.com>
2016-10-29 19:54:13 +0300

    Created function get_password_strength(password) in module password_strength.py which evaluates password from 1 to 10. Write some usefull information about it in README.md

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # 6_password_strength
+
+## Synopsis
+
+Script helps you to understand which password is great. It evaluates password from one to ten. One means - worst password ever. Ten means - you can use that password to protect you personal data.
+
+## Quick start
+
+ - Run password_strength.py `python3.5 password_strength.py`
+ - **Input**: write password
+ - **Output**: password score from one to ten
+
+ ## Module functions
+
+ - 'get_password_strength(password)' - evaluates password from one to ten. Return `int` in range from 1 to 10.
--- password_strength.py
+++ password_strength.py
@@ -1,8 +1,18 @@
-
+import re
 
 def get_password_strength(password):
-    pass
+    patterns = [r'[A-Z]', r'[a-z]', r'[0-9]', r'[^a-zA-Z0-9]']
+    pwd_length = len(password)
+    
+    pwd_strength = int(pwd_length > 7) + int(pwd_length > 11)
+    for x in (len(re.findall(pattern, password)) for pattern in patterns):
+        pwd_strength += int(x > 0) + int(x > 2)
+
+    return pwd_strength
+
 
 
 if __name__ == '__main__':
-    pass
+    password_strength = get_password_strength(input('Enter password: '))
+    print('Password strength is {0} out of 10'.format(password_strength))
+    
